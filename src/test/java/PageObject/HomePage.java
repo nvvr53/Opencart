d4bc524404428202c3b0b3ce032dc7b1871f7d3a
Vishnuvardhan <nvvr53@gmail.com>
2023-12-13 22:23:07 +0530

    Framework added

---
+++ src/test/java/PageObject/HomePage.java
@@ -0,0 +1,40 @@
+package PageObject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.WebElement;
+
+
+public class HomePage extends BasePage{
+		
+	public HomePage(WebDriver driver) {
+		
+		super(driver);
+	}
+
+	// Elements
+	@FindBy(xpath = "//span[text()='My Account']")
+	WebElement lnkMyaccount;
+
+	@FindBy(linkText = "Register")
+	WebElement lnkRegister;
+
+	
+	@FindBy(linkText = "Login")   // Login link added in step6
+	WebElement linkLogin;
+	
+	
+	
+	// Action Methods
+	public void clickMyAccount() {
+		lnkMyaccount.click();
+	}
+
+	public void clickRegister() {
+		lnkRegister.click();
+	}
+
+	public void clickLogin() {
+		linkLogin.click();
+	}
+}
